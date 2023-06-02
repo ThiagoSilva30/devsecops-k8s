b1868a88449b53b9c35872cf01fd6519fb9bbefe
Thiago Silva <thiagoguita@hotmail.com>
2023-06-02 17:25:05 -0300

    Update NumericController.java

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -22,7 +22,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps";
+			return "Kubernetes DevSecOps Thiago Gama da Silva";
 		}
 
 		@GetMapping("/compare/{value}")
